dd4a595252a2a625b1d627786eaa84d979822c1d
DIWAKAR <didk9675@gmail.com>
2023-06-23 21:12:43 +0530

    f

--- frontend/components/form.js
+++ frontend/components/form.js
@@ -5,8 +5,8 @@ const Form = () => {
         <div className=" max-w-2xl  m-auto mt-40 ">
             <div className="flex w-2xl items-center justify-center">
             
-                <a href="./data" class="relative cursor-pointer inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white text-white focus:ring-4 focus:outline-none  focus:ring-purple-800">
-                    <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                <a href="./data" className="relative cursor-pointer inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white text-white focus:ring-4 focus:outline-none  focus:ring-purple-800">
+                    <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-gray-900 rounded-md group-hover:bg-opacity-0">
                         Create CryptoAPI Wallet
                     </span>
                 </a>
--- frontend/pages/data/index.js
+++ frontend/pages/data/index.js
@@ -2,11 +2,11 @@ import React, { useState } from "react";
 import Nav from "../../components/nav";
 import Footer from "../../components/footer";
 
-export const getStaticProps = async () => {
-  const res = await fetch(`http://localhost:3000/api/walletAddress`); // change the wrl according to domain
-  const walletAddress = await res.json();
-  return { props: { walletAddress } };
-};
+// export const getStaticProps = async () => {
+//   const res = await fetch(`http://localhost:3000/api/walletAddress`); // change the wrl according to domain
+//   const walletAddress = await res.json();
+//   return { props: { walletAddress } };
+// };
 
 const TransactionData = ({ walletAddress }) => {
   console.log(walletAddress);
@@ -24,7 +24,7 @@ const TransactionData = ({ walletAddress }) => {
                   Wallet Address :-
                 </h5>
                 <p className=" text-sm text-gray-300">
-                  {walletAddress.address}
+                  {/* {walletAddress.address} */}
                 </p>
                 <br />
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-300">
