d3870ac345c5f573f33060903d2d678e110e4e61
DIWAKAR <didk9675@gmail.com>
2023-06-26 20:18:06 +0530

    last

--- frontend/components/form.js
+++ frontend/components/form.js
@@ -6,8 +6,8 @@ const Form = () => {
             <div className="flex w-2xl items-center justify-center">
             
                 <a href="./data" className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white text-white focus:ring-4 focus:outline-none focus:ring-blue-800">
-                    <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-                    Create Ethereum Wallet
+                    <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                    Create Ethereum Wallet 
                     </span>
                 </a>
 
